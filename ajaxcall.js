665684764c2290cf7f951d8775020169212a9b94
alexng88 <alexng88@gmail.com>
2017-02-07 00:48:13 -0500

    nodejs code

---
+++ ajaxcall.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    console.log('request received');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+   // res.write("Ajax call");
+     //res.end("end call");
+  res.end('_testcb(\'{"message": "Hello world!"}\')');
+}).listen(8000);
---
+++ static.js
@@ -0,0 +1,6 @@
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(9999, function(req, res){
+  console.log('request received');
+    console.log('Server running on 9999...');
+});
